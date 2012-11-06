946b374a2e796a6b007ea6e74fe7e7dd058f3427
leyner <leyner@leyner-M2421>
2012-11-05 22:57:13 -0430

    Completado metodo Init Agregado metodo para desactivar y activar componentes.

--- presupuestoNardi/src/controller/FrmIndexCtrl.java
+++ presupuestoNardi/src/controller/FrmIndexCtrl.java
@@ -1,25 +1,16 @@
 package controller;
 
-import java.util.ArrayList;
+import general.Validate;
+
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import org.zkoss.bind.Validator;
+import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.Init;
-import org.zkoss.bind.annotation.NotifyChange;
-import org.zkoss.zk.ui.select.SelectorComposer;
-import org.zkoss.zk.ui.select.annotation.Listen;
-import org.zkoss.zk.ui.select.annotation.Wire;
-import org.zkoss.zul.Button;
-import org.zkoss.zul.Checkbox;
-import org.zkoss.zul.Combobox;
-import org.zkoss.zul.Datebox;
-import org.zkoss.zul.Intbox;
-import org.zkoss.zul.Messagebox;
-import org.zkoss.zul.Radiogroup;
-import org.zkoss.zul.Spinner;
-import org.zkoss.zul.Textbox;
-import org.zkoss.zul.Window;
+import org.zkoss.zul.impl.InputElement;
 
 import dao.DaoBasicData;
 import dao.DaoBudget;
@@ -28,196 +19,10 @@ import database.Budget;
 
 /**
  * @author leyner.castillo
- *
+ * 
  */
-public class FrmIndexCtrl{
+public class FrmIndexCtrl {
 
-/*	@Wire
-	private Button btnCancel;
-	@Wire
-	private Button btnPrint;
-	@Wire
-	private Button btnSave;
-	@Wire
-	private Button btnSend;
-	@Wire
-	private Textbox txtBComment;
-	@Wire
-	private Radiogroup radBFirefighterKeychain;
-	@Wire
-	private Radiogroup radBAccessSytem;
-	@Wire
-	private Radiogroup radBPhone;
-	@Wire
-	private Radiogroup radBIntercom;
-	@Wire
-	private Radiogroup radBGomSystem;
-	@Wire
-	private Radiogroup radBSpeechSynthesizer;
-	@Wire
-	private Radiogroup radBLoadLimiter;
-	@Wire
-	private Radiogroup radBLightCurtain;
-	@Wire
-	private Radiogroup radBDisplayPlaceFloor;
-	@Wire
-	private Combobox cmbBFloorDisplay;
-	@Wire
-	private Combobox cmbBBoothDisplay;
-	@Wire
-	private Spinner spnBAntivandalismFloor;
-	@Wire
-	private Spinner spnBAntivandalismPB;
-	@Wire
-	private Spinner spnBAntivandalism;
-	@Wire
-	private Spinner spnBBraile37Floor;
-	@Wire
-	private Spinner spnBBraile37PB;
-	@Wire
-	private Spinner spnBBraile37;
-	@Wire
-	private Spinner spnBSistelWArrowFloor;
-	@Wire
-	private Spinner spnBSistelWArrowPB;
-	@Wire
-	private Spinner spnBSistelWArrow;
-	@Wire
-	private Spinner spnBSistelWDisplayFloor;
-	@Wire
-	private Spinner spnBSistelWDisplayPB;
-	@Wire
-	private Spinner spnBSistelWDisplay;
-	@Wire
-	private Radiogroup radBHallButton;
-	@Wire
-	private Combobox cmbBBoothButton;
-	@Wire	
-	private Combobox cmbBControlType;
-	@Wire
-	private Textbox txtBHammeredBrown;
-	@Wire
-	private Textbox txtBHammeredGray;
-	@Wire
-	private Checkbox chbxBHammeredGray;
-	@Wire
-	private Textbox txtBStainlessSteel;
-	@Wire
-	private Checkbox chbxBStainlessSteel;
-	@Wire
-	private Button btnNewHeight;
-	@Wire
-	private Combobox cmbBHeight;
-	@Wire
-	private Button btnNewFreeAdmission;
-	@Wire
-	private Combobox cmbBFreeAdmission;
-	@Wire
-	private Combobox cmbBDoorframeType;
-	@Wire
-	private Combobox spnBDoorSystem;
-	@Wire
-	private Combobox spnBDoorType;
-	@Wire
-	private Spinner spnBDoorOfNumber;
-	@Wire
-	private Combobox cmbBFan;
-	@Wire
-	private Combobox cmbBFloorType;
-	@Wire
-	private Combobox cmbBMirror;
-	@Wire
-	private Combobox cmbBRailing;
-	@Wire
-	private Textbox txtBDesignSpecialComment;
-	@Wire
-	private Combobox cmbBButtonType;
-	@Wire
-	private Combobox cmbBRoofType;
-	@Wire
-	private Checkbox chbxBDesignSpecial;
-	@Wire
-	private Combobox cmbBDesignP26;
-	@Wire
-	private Combobox cmbBDesign1357;
-	@Wire
-	private Spinner spnBBottomHole;
-	@Wire
-	private Spinner spnBFossa;
-	@Wire
-	private Spinner spnBWidthHole;
-	@Wire
-	private Spinner spnBOnTour;
-	@Wire
-	private Spinner spnBTour;
-	@Wire
-	private Checkbox chbxBStopSequenceOdd;
-	@Wire
-	private Spinner spnBStopNumber;
-	@Wire
-	private Checkbox chbxBStopSequencePar;
-	@Wire
-	private Combobox cmbBManeuverType;
-	@Wire
-	private Checkbox chbxBStopSequenceContinuous;
-	@Wire
-	private Combobox cmbBHourMachine;
-	@Wire
-	private Combobox cmbBVoltageLighting;
-	@Wire
-	private Combobox cmbBFrequency;
-	@Wire
-	private Button btnNewSpeed;
-	@Wire
-	private Combobox cmbBSpeed;
-	@Wire
-	private Combobox cmbBElectricityType;
-	@Wire
-	private Combobox cmbBEmbarque;
-	@Wire
-	private Combobox cmbBMachineType;
-	@Wire
-	private Radiogroup radBMotorTraction;
-	@Wire
-	private Button btnNewElevatorCapacitance;
-	@Wire
-	private Combobox cmbBElevatorCapacitance;
-	@Wire
-	private Spinner cmbBMotorQuantity;
-	@Wire
-	private Combobox cmbBElevatorType;
-	@Wire
-	private Spinner spnBElevatorQuantity;
-	@Wire
-	private Combobox cmbBBuildingType;
-	@Wire
-	private Radiogroup radPlaneP;
-	@Wire
-	private Radiogroup radPlaneC;
-	@Wire
-	private Textbox txtBContactName;
-	@Wire
-	private Textbox txtBEmail;
-	@Wire
-	private Textbox txtBcontactPhone;
-	@Wire
-	private Textbox txtBConstructionCity;
-	@Wire
-	private Textbox txtBConstructionAddress;
-	@Wire
-	private Textbox txtBSeller;
-	@Wire
-	private Textbox txtBConstructionNumber;
-	@Wire
-	private Radiogroup radBType;
-	@Wire
-	private Textbox txtBConstruction;
-	@Wire
-	private Datebox dtbBDate;
-	@Wire
-	private Textbox txtBPartnerName;
-	@Wire
-	private Intbox intBNumber;*/
 	private List<Basicdata> listBType;
 	private Basicdata selectedBType;
 	private List<Basicdata> listElevatorType;
@@ -244,7 +49,286 @@ public class FrmIndexCtrl{
 	private Basicdata selectedDesign1357;
 	private List<Basicdata> listDesignP26;
 	private Basicdata selectedDesignP26;
-		
+	private List<Basicdata> listRoofType;
+	private Basicdata selectedRoofType;
+	private List<Basicdata> listButtonType;
+	private Basicdata selectedButtonType;
+	private List<Basicdata> listRailing;
+	private Basicdata selectedRailing;
+	private List<Basicdata> listMirror;
+	private Basicdata selectedMirror;
+	private List<Basicdata> listFloorType;
+	private Basicdata selectedFloorType;
+	private List<Basicdata> listFan;
+	private Basicdata selectedFan;
+	private List<Basicdata> listDoorType;
+	private Basicdata selectedDoorType;
+	private List<Basicdata> listDoorSystem;
+	private Basicdata selectedDoorSystem;
+	private List<Basicdata> listDoorframeType;
+	private Basicdata selectedDoorframeType;
+	private List<Basicdata> listFreeAdmission;
+	private Basicdata selectedFreeAdmission;
+	private List<Basicdata> listHeight;
+	private Basicdata selectedHeight;
+	private List<Basicdata> listControlType;
+	private Basicdata selectedControlType;
+	private List<Basicdata> listBoothButton;
+	private Basicdata selectedBoothButton;
+	private List<Basicdata> listBoothDisplay;
+	private Basicdata selectedBoothDisplay;
+	private List<Basicdata> listFloorDisplay;
+	private Basicdata selectedFloorDisplay;
+	private Budget budget;
+
+	public Budget getBudget() {
+		return budget;
+	}
+
+	public void setBudget(Budget budget) {
+		this.budget = budget;
+	}
+
+	public List<Basicdata> getListFloorDisplay() {
+		return listFloorDisplay;
+	}
+
+	public void setListFloorDisplay(List<Basicdata> listFloorDisplay) {
+		this.listFloorDisplay = listFloorDisplay;
+	}
+
+	public Basicdata getSelectedFloorDisplay() {
+		return selectedFloorDisplay;
+	}
+
+	public void setSelectedFloorDisplay(Basicdata selectedFloorDisplay) {
+		this.selectedFloorDisplay = selectedFloorDisplay;
+	}
+
+	public List<Basicdata> getListBoothDisplay() {
+		return listBoothDisplay;
+	}
+
+	public void setListBoothDisplay(List<Basicdata> listBoothDisplay) {
+		this.listBoothDisplay = listBoothDisplay;
+	}
+
+	public Basicdata getSelectedBoothDisplay() {
+		return selectedBoothDisplay;
+	}
+
+	public void setSelectedBoothDisplay(Basicdata selectedBoothDisplay) {
+		this.selectedBoothDisplay = selectedBoothDisplay;
+	}
+
+	public List<Basicdata> getListBoothButton() {
+		return listBoothButton;
+	}
+
+	public void setListBoothButton(List<Basicdata> listBoothButton) {
+		this.listBoothButton = listBoothButton;
+	}
+
+	public Basicdata getSelectedBoothButton() {
+		return selectedBoothButton;
+	}
+
+	public void setSelectedBoothButton(Basicdata selectedBoothButton) {
+		this.selectedBoothButton = selectedBoothButton;
+	}
+
+	public List<Basicdata> getListControlType() {
+		return listControlType;
+	}
+
+	public void setListControlType(List<Basicdata> listControlType) {
+		this.listControlType = listControlType;
+	}
+
+	public Basicdata getSelectedControlType() {
+		return selectedControlType;
+	}
+
+	public void setSelectedControlType(Basicdata selectedControlType) {
+		this.selectedControlType = selectedControlType;
+	}
+
+	public List<Basicdata> getListHeight() {
+		return listHeight;
+	}
+
+	public void setListHeight(List<Basicdata> listHeight) {
+		this.listHeight = listHeight;
+	}
+
+	public Basicdata getSelectedHeight() {
+		return selectedHeight;
+	}
+
+	public void setSelectedHeight(Basicdata selectedHeight) {
+		this.selectedHeight = selectedHeight;
+	}
+
+	public List<Basicdata> getListFreeAdmission() {
+		return listFreeAdmission;
+	}
+
+	public void setListFreeAdmission(List<Basicdata> listFreeAdmission) {
+		this.listFreeAdmission = listFreeAdmission;
+	}
+
+	public Basicdata getSelectedFreeAdmission() {
+		return selectedFreeAdmission;
+	}
+
+	public void setSelectedFreeAdmission(Basicdata selectedFreeAdmission) {
+		this.selectedFreeAdmission = selectedFreeAdmission;
+	}
+
+	public List<Basicdata> getListDoorframeType() {
+		return listDoorframeType;
+	}
+
+	public void setListDoorframeType(List<Basicdata> listDoorframeType) {
+		this.listDoorframeType = listDoorframeType;
+	}
+
+	public Basicdata getSelectedDoorframeType() {
+		return selectedDoorframeType;
+	}
+
+	public void setSelectedDoorframeType(Basicdata selectedDoorframeType) {
+		this.selectedDoorframeType = selectedDoorframeType;
+	}
+
+	public List<Basicdata> getListDoorSystem() {
+		return listDoorSystem;
+	}
+
+	public void setListDoorSystem(List<Basicdata> listDoorSystem) {
+		this.listDoorSystem = listDoorSystem;
+	}
+
+	public Basicdata getSelectedDoorSystem() {
+		return selectedDoorSystem;
+	}
+
+	public void setSelectedDoorSystem(Basicdata selectedDoorSystem) {
+		this.selectedDoorSystem = selectedDoorSystem;
+	}
+
+	public List<Basicdata> getListDoorType() {
+		return listDoorType;
+	}
+
+	public void setListDoorType(List<Basicdata> listDoorType) {
+		this.listDoorType = listDoorType;
+	}
+
+	public Basicdata getSelectedDoorType() {
+		return selectedDoorType;
+	}
+
+	public void setSelectedDoorType(Basicdata selectedDoorType) {
+		this.selectedDoorType = selectedDoorType;
+	}
+
+	public List<Basicdata> getListFan() {
+		return listFan;
+	}
+
+	public void setListFan(List<Basicdata> listFan) {
+		this.listFan = listFan;
+	}
+
+	public Basicdata getSelectedFan() {
+		return selectedFan;
+	}
+
+	public void setSelectedFan(Basicdata selectedFan) {
+		this.selectedFan = selectedFan;
+	}
+
+	public List<Basicdata> getListFloorType() {
+		return listFloorType;
+	}
+
+	public void setListFloorType(List<Basicdata> listFloorType) {
+		this.listFloorType = listFloorType;
+	}
+
+	public Basicdata getSelectedFloorType() {
+		return selectedFloorType;
+	}
+
+	public void setSelectedFloorType(Basicdata selectedFloorType) {
+		this.selectedFloorType = selectedFloorType;
+	}
+
+	public List<Basicdata> getListMirror() {
+		return listMirror;
+	}
+
+	public void setListMirror(List<Basicdata> listMirror) {
+		this.listMirror = listMirror;
+	}
+
+	public Basicdata getSelectedMirror() {
+		return selectedMirror;
+	}
+
+	public void setSelectedMirror(Basicdata selectedMirror) {
+		this.selectedMirror = selectedMirror;
+	}
+
+	public List<Basicdata> getListRailing() {
+		return listRailing;
+	}
+
+	public void setListRailing(List<Basicdata> listRailing) {
+		this.listRailing = listRailing;
+	}
+
+	public Basicdata getSelectedRailing() {
+		return selectedRailing;
+	}
+
+	public void setSelectedRailing(Basicdata selectedRailing) {
+		this.selectedRailing = selectedRailing;
+	}
+
+	public List<Basicdata> getListButtonType() {
+		return listButtonType;
+	}
+
+	public void setListButtonType(List<Basicdata> listButtonType) {
+		this.listButtonType = listButtonType;
+	}
+
+	public Basicdata getSelectedButtonType() {
+		return selectedButtonType;
+	}
+
+	public void setSelectedButtonType(Basicdata selectedButtonType) {
+		this.selectedButtonType = selectedButtonType;
+	}
+
+	public List<Basicdata> getListRoofType() {
+		return listRoofType;
+	}
+
+	public void setListRoofType(List<Basicdata> listRoofType) {
+		this.listRoofType = listRoofType;
+	}
+
+	public Basicdata getSelectedRoofType() {
+		return selectedRoofType;
+	}
+
+	public void setSelectedRoofType(Basicdata selectedRoofType) {
+		this.selectedRoofType = selectedRoofType;
+	}
+
 	public List<Basicdata> getListDesignP26() {
 		return listDesignP26;
 	}
@@ -444,7 +528,7 @@ public class FrmIndexCtrl{
 	public void setSelectedBType(Basicdata selectedBType) {
 		this.selectedBType = selectedBType;
 	}
-	
+
 	public List<Basicdata> getListBType() {
 		return listBType;
 	}
@@ -453,25 +537,64 @@ public class FrmIndexCtrl{
 		this.listBType = listBType;
 	}
 
+	/**
+	 * Método que inicializa la pantalla frmIndex. Se ejecuta antes de
+	 * finalizar la carga del archivo DOM que muestra el navegador.
+	 * 
+	 * Inicializa cada una de la variables insertadas en zul.
+	 */
 	@Init
-	public void init(){
+	public void init() {
 		DaoBasicData daoBasicData = new DaoBasicData();
+		DaoBudget daoBudget = new DaoBudget();
+		budget = new Budget();
+		int budgetSizes = daoBudget.list(Budget.class).size();
+		if (budgetSizes == 0)
+			budget.setNumber(1);
+		else
+			budget.setNumber(daoBudget.list(Budget.class).get(daoBudget.list(Budget.class).size()-1).getNumber()+1);
+		budget.setDate(new Date());
 		listBType = daoBasicData.findByDescription("BUDGET", "BUILDING TYPE");
-		listElevatorType = daoBasicData.findByDescription("BUDGET", "ELEVATOR TYPE");
-		listElevatorCapa = daoBasicData.findByDescription("BUDGET", "ELEVATOR CAPACITANCE");
-		listMachineType = daoBasicData.findByDescription("BUDGET", "MACHINE TYPE");
-		listBEmbarque = daoBasicData.findByDescription("BUDGET", "BUILDING EMBARQUE");
-		listElectricityType = daoBasicData.findByDescription("BUDGET", "ELECTRICITY TYPE");
+		listElevatorType = daoBasicData.findByDescription("BUDGET","ELEVATOR TYPE");
+		listElevatorCapa = daoBasicData.findByDescription("BUDGET","ELEVATOR CAPACITANCE");
+		listMachineType = daoBasicData.findByDescription("BUDGET","MACHINE TYPE");
+		listBEmbarque = daoBasicData.findByDescription("BUDGET","BUILDING EMBARQUE");
+		listElectricityType = daoBasicData.findByDescription("BUDGET","ELECTRICITY TYPE");
 		listSpeed = daoBasicData.findByDescription("BUDGET", "SPEED");
 		listFrequency = daoBasicData.findByDescription("BUDGET", "FREQUENCY");
-		listVoltageLighting = daoBasicData.findByDescription("BUDGET", "VOLTAGE LIGHTING");
-		listHourMachine = daoBasicData.findByDescription("BUDGET", "HOUR MACHINE");
-		listManeuverType = daoBasicData.findByDescription("BUDGET", "MANEUVER TYPE");
+		listVoltageLighting = daoBasicData.findByDescription("BUDGET","VOLTAGE LIGHTING");
+		listHourMachine = daoBasicData.findByDescription("BUDGET","HOUR MACHINE");
+		listManeuverType = daoBasicData.findByDescription("BUDGET","MANEUVER TYPE");
 		listDesign1357 = daoBasicData.findByDescription("BUDGET", "DESIGN 1357");
 		listDesignP26 = daoBasicData.findByDescription("BUDGET", "DESIGN P26");
+		listRoofType = daoBasicData.findByDescription("BUDGET", "ROOF TYPE");
+		listButtonType = daoBasicData.findByDescription("BUDGET", "BUTTON TYPE");
+		listRailing = daoBasicData.findByDescription("BUDGET", "RAILING");
+		listMirror = daoBasicData.findByDescription("BUDGET", "MIRROR");
+		listFloorType = daoBasicData.findByDescription("BUDGET", "FLOOR TYPE");
+		listFan = daoBasicData.findByDescription("BUDGET", "FAN");
+		listDoorType = daoBasicData.findByDescription("BUDGET", "DOOR TYPE");
+		listDoorSystem = daoBasicData.findByDescription("BUDGET", "DOOR SYSTEM");
+		listDoorframeType = daoBasicData.findByDescription("BUDGET","DOOR FRAME TYPE");
+		listFreeAdmission = daoBasicData.findByDescription("BUDGET","FREE ADMISSION");
+		listHeight = daoBasicData.findByDescription("BUDGET", "HEIGHT");
+		listControlType = daoBasicData.findByDescription("BUDGET","CONTROL TYPE");
+		listBoothButton = daoBasicData.findByDescription("BUDGET","BOOTH BUTTON");
+		listBoothDisplay = daoBasicData.findByDescription("BUDGET","BOOTH DISPLAY");
+		listFloorDisplay = daoBasicData.findByDescription("BUDGET","FLOOR DISPLAY");
+	}
+
+	@Command
+	public void clickToEnableToDisable(@BindingParam("Component") InputElement element) {
+		if(element.isDisabled())
+			element.setDisabled(false);
+		else
+			element.setDisabled(true);
 	}
 	
-	@Command
-	public void clickBuildinType(){
-	}
+	/*@Command
+	public void save(){
+		Validate validate = new Validate();
+		if (budget.getPartnername().isEmpty())
+	}*/
 }
