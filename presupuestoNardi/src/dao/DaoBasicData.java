39a16d8c1053d28848cbf8de2d9bf0dc2a444e80
leyner.castillo <leyner.castillo@02-COM-BQT-TIC.comlago.local>
2012-11-07 16:57:01 -0430

    Modificado metodo findbyDescription por findByName y se coloca listByName

--- presupuestoNardi/src/dao/DaoBasicData.java
+++ presupuestoNardi/src/dao/DaoBasicData.java
@@ -12,14 +12,26 @@ import hibernateConnections.GenericDao;
 
 public class DaoBasicData extends GenericDao<Basicdata> {
 
-	public List<Basicdata> findByDescription(String table, String field) {
+	public List<Basicdata> listByField(String table, String field) {
 		Transaction transaction = currentSession().beginTransaction();
 		Criteria criteria = currentSession().createCriteria(Basicdata.class);
 		criteria.add(Restrictions.eq("status", 'A'));
 		criteria.add(Restrictions.eq("editable", false));
-		criteria.add(Restrictions.eq("description", field));
+		criteria.add(Restrictions.eq("field", field));
 		criteria.add(Restrictions.eq("database", table));
 		criteria.addOrder(Order.asc("priority"));
 		return criteria.list();
+	}
+
+	public Basicdata findByName(String table, String field, String name) {
+		Transaction transaction = currentSession().beginTransaction();
+		Criteria criteria = currentSession().createCriteria(Basicdata.class);
+		criteria.add(Restrictions.eq("status", 'A'));
+		criteria.add(Restrictions.eq("editable", false));
+		criteria.add(Restrictions.eq("field", field));
+		criteria.add(Restrictions.eq("name", name));
+		criteria.add(Restrictions.eq("database", table));
+		criteria.addOrder(Order.asc("priority"));
+		return (Basicdata)criteria.uniqueResult();
 	}
 }
---
+++ presupuestoNardi/src/dao/DaoDataBasicManyToMany.java
@@ -0,0 +1,8 @@
+package dao;
+
+import database.Databasicmanytomany;
+import hibernateConnections.GenericDao;
+
+public class DaoDataBasicManyToMany extends GenericDao<Databasicmanytomany> {
+
+}
