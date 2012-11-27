c4e7726d2682320fd32c2433db43e9024e88ec8a
leyner.castillo <leyner.castillo@02-COM-BQT-TIC.comlago.local>
2012-11-27 16:58:45 -0430

    Agregado nuevo metodo, que retorna una lista de budgets ordenador por cierto campo.

--- presupuestoNardi/src/dao/DaoBudget.java
+++ presupuestoNardi/src/dao/DaoBudget.java
@@ -1,8 +1,22 @@
 package dao;
 
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+
+import database.Basicdata;
 import database.Budget;
 import hibernateConnections.GenericDao;
 
 public class DaoBudget extends GenericDao<Budget> {
-	
+
+	public List<Budget> listOrderBudgetNumber(String field) {
+		Transaction transaction = currentSession().beginTransaction();
+		Criteria criteria = currentSession().createCriteria(Budget.class);
+		criteria.addOrder(Order.asc(field));
+		return criteria.list();
+	}
 }
